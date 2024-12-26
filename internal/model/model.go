ba968d8449c943ea85d4994d6443e217440e0d8e
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-12-26 18:21:11 +0000

    add: new types found in model.go

---
+++ internal/model/model.go
@@ -0,0 +1,19 @@
+package model
+
+import "database/sql"
+
+type Client struct {
+	Connection *sql.DB
+}
+
+type Note struct {
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+	UserId string `json:"userId"`
+}
+
+type Response struct {
+	Message     string `json:"message"`
+	CreatedNote Note   `json:"createdNote"`
+}
