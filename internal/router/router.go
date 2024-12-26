17c0d01291f7d4a828bd3c5633a76b83c2dc0c96
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-12-26 18:21:29 +0000

    add: start to adjust and make the router look cleaner

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,19 +1,24 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"florent-haxhiu/todoInGo/internal/controllers"
 )
 
 func Router() *chi.Mux {
 	r := chi.NewRouter()
+
 	r.Use(middleware.Logger)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Recoverer)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+
+	r.Route("/note", func(r chi.Router) {
+		r.Route("/{noteId}", func(r chi.Router) {
+			r.Get("/", controllers.GetNote)
+		})
+		r.Post("/", controllers.PostNote)
 	})
 
 	return r
