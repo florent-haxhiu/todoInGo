4c7c511e5d1f1ccb24e50934b71d528058db1eb1
Florent Haxhiu <florenthaxhiu03@gmail.com>
2024-12-28 19:05:08 +0000

    add: start auth workflow

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,55 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/golang-jwt/jwt/v5"
+
+	db "florent-haxhiu/todoInGo/internal/database"
+	"florent-haxhiu/todoInGo/internal/model"
 )
 
-func generateToken() {}
+type AuthResources struct{}
+
+func (ar AuthResources) Login(w http.ResponseWriter, r *http.Request) {}
+
+func (ar AuthResources) Register(w http.ResponseWriter, r *http.Request) {
+	var user model.UserRegister
+
+	fmt.Println(user)
+	body := json.NewDecoder(r.Body)
+	body.DisallowUnknownFields()
+
+	err := body.Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnprocessableEntity)
+		return
+	}
+
+	done, err := db.SaveUserToDB(ar.saltPassword(user))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusTeapot)
+		return
+	}
+}
+
+func (ar AuthResources) generateToken(user model.UserPassHashed) *jwt.Token {
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"username": user.Username,
+		"password": user.Password,
+	})
+
+	return token
+}
+
+func (ar AuthResources) saltPassword(user model.UserRegister) model.UserPassHashed {
+	var hashed model.UserPassHashed
+
+	print(user)
+
+	return hashed
+}
+
+func (ar AuthResources) unhashPassword() {}
