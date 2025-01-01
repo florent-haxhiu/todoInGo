d74a1d93a10beaa48180b5cecd878fcb51928129
Florent Haxhiu <florenthaxhiu03@gmail.com>
2025-01-01 07:59:37 +0000

    add: use dotenv to pass down a secret

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/joho/godotenv"
 
 	"florent-haxhiu/todoInGo/internal/router"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Server now up and running")
 	http.ListenAndServe(":3000", router.Router())
 }
