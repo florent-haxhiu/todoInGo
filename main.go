864ed608f8948a91db731768c1250725c8516a26
Florent Haxhiu <florenthaxhiu03@gmail.com>
2025-05-20 13:03:40 +0100

    add: init logger on root file

--- main.go
+++ main.go
@@ -6,15 +6,28 @@ import (
 
 	"github.com/joho/godotenv"
 
+	"florent-haxhiu/todoInGo/internal/logger"
 	"florent-haxhiu/todoInGo/internal/router"
 )
 
 func main() {
+	logger.InitializeDefault()
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
-	fmt.Println("Server now up and running")
-	http.ListenAndServe(":3000", router.Router())
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: router.Router(),
+	}
+
+	logger.InfoMsg("Server starting", "addr", ":3000")
+	err = server.ListenAndServe()
+
+	if err != nil {
+		logger.ErrorMsg("Server failed to start", "error", err)
+	}
 }
